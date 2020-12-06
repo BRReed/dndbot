d67dc8e4752ea85e2f3c01ec06cd1e79bea99045
Brian R Reed <b.r.reed@gmail.com>
2020-12-05 17:28:44 -0800

    hangman fully functional in discord

--- dndbot_discord.py
+++ dndbot_discord.py
@@ -9,13 +9,19 @@ import hangmanbot
 bot = commands.Bot(command_prefix = '.')
 
 
-
 @bot.event
 async def on_ready():
+    '''
+    Prints to console when bot is initialized
+    '''
     print('Bot is ready.')
 
 @bot.command(name='shutdown')
 async def shutdown(message):
+    '''
+    shutdown from authorized user
+    action: shuts bot down
+    '''
     if str(message.author.id) == '275827627502075905':
         print('bot closed')
         await bot.close()
@@ -26,28 +32,28 @@ async def shutdown(message):
 # available commands in discord
 @bot.command(name='commands')
 async def user_commands(ctx):
+    '''
+    Prints available commands to channel
+    '''
     await ctx.send(
         """
 **.dicegame** - Whoever rolls higher wins
+**.combat** - Play a game of DND combat (caution - boring in current state)
 **.commands** - Get a list of commands
-**.create** - Make a character
+**.create** - Make a DND character 
+**.hangman** - Play a game of hangman
 **.wins** - Show the wins and losses of your character
-**.roll** - roll a 1 - 1000 die 
-
         """
     )
-
-@bot.command(name='roll')
-async def roll(ctx):
-    current_roll = dndbot.roll_die(1, 1000)
-    await ctx.channel.send(f'**{ctx.author}** rolled **{current_roll}**')
-    if current_roll == 420:
-        await ctx.channel.send(f'OH SHIT!!! **420 BLAZE IT**')
 
 # simple dice game used to test different aspects of the discord api
 @bot.command(name='dicegame')
 async def dice_game(ctx):
-    print('we made it to dice game function')
+    '''
+    Plays a game of higher/lower with 2 6 sided dice 
+    Initiator can choose to play against either comp or another user
+    '''
+    
     await ctx.channel.send("type '1' to play against computer,"+
                            "'2' to wait for another player")
     choice = await bot.wait_for('message',
@@ -429,8 +435,14 @@ Enter '2' to run
             p_player = 0
 
 
+
+
 @bot.command(name='hangman')
 async def hangman(ctx):
+    '''
+    Command initiator plays a game of hangman against the computer
+    
+    '''
     game = hangmanbot.HangMan()
     warnings = 3
     guesses = 6
@@ -441,41 +453,53 @@ Your secret word is **{len(game.secret_word)}** letters long!
 {game.secret_word}
     ''')
     while True:
+        game.get_guessed_word()
         await ctx.send(f'''
-You have {guesses} guesses left!
-Your available letters are: {game.get_available_letters()}
-Enter \'*\' to see all possible matches in the words list
+You have **{guesses}** guesses left!
+Your available letters are: **{game.get_available_letters()}**
+Enter **\'*\'** to see all possible matches in the words list
+Your guessed word is 
+`{game.word_guessed}`
         ''')
         letter = await bot.wait_for('message',
         check=lambda message: message.author == ctx.author, timeout = 500)
         if letter.content.lower() == '*':
             game.show_possible_matches()
-            if len(game.wl) > 2000:
-                # split into separate strings, by half, at closest comma
-                # do this in hangmanbot.py and for loop here based on return num
-                # strings split into 2k slices num += 1 
-                pass 
-            else:
-                await ctx.send(f''' non-butt stuff
-                {game.wl}
+            try:
+                await ctx.send(f'''
+Possible words are:
+**{game.wl}**
                 ''')
+                # figure out how to actually catch this
+            except commands.CommandInvokeError: 
+                await ctx.send('Sorry, The list of possible words isn\'t ',
+                               'small enough. Please guess more and try again.')
+
         elif letter.content.lower() in game.get_available_letters():
             game.letters_guessed.append(letter.content.lower())
+            if letter.content.lower() not in game.secret_word:
+                guesses -= 1
         else:
-            await ctx.send('''
+            warnings -= 1
+            await ctx.send(f'''
 Please choose a letter from the list
+You have **{warnings}** warnings left
             ''')
-            continue
-        await ctx.send(f''' butt stuff
-{game.letters_guessed}
-        ''')
-
-
-
-    # continue here. need to reformat hangman.py as we go
-    # to make it act as a class
-
-
-
+            if warnings == 0:
+                break
+            else:
+                continue
+        if guesses == 0:
+            await ctx.send(f'''
+Sorry you ran out of guesses. 
+Your secret word was **{game.secret_word}**
+            ''')
+            break
+        if game.is_word_guessed() == True:
+            await ctx.send(f'''
+Congratulations! You win! Your word was **{game.secret_word}**
+You had **{guesses}** guesses remaining
+            ''')
+            break
 
 bot.run(dndbot_token.token)
